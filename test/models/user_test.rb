ebda3d19ad407258a4e269de927014ace1c82635
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-01 02:12:55 +0900

    Userモデルのnameのvalidationを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "mofumofu")
+  end
+
+  test "validでなくてはならない" do
+    assert @user.valid?
+  end
+
+  test "nameは存在していないとならない" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "nameの長さは50文字以下でないとならない" do
+    @user.name = "a"*51
+    assert_not @user.valid?
+  end
+
+  test "nameは一意でないとならない" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
