b76738803c7d2b13b85597542cc2c0da30e286f6
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-01 05:01:00 +0900

    タイトルページの文章を作り，ルートから飛べるようにした
    ※もっと小さい粒度でcommitしたほうが良かった．

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages#home'
   resources :users, :except => [:edit, :update, :destroy]
   get 'signup', to: 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  def setup
+    @base_title = "きみもしらない、いいところ。"
+  end
+
+  test "should get root" do
+    get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
 end
