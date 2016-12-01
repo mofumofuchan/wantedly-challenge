a776abe3bcbfb1c0e1d0bb7bd5a130991d7c37db
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-02 07:46:39 +0900

    GoodPointモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201050744) do
+ActiveRecord::Schema.define(version: 20161201051446) do
+
+  create_table "good_points", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "said_good_points", force: :cascade do |t|
     t.integer  "from",          null: false
