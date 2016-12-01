680dee1480101fd443fca473b394b3d172124b61
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-01 12:52:49 +0900

    ログイン機構のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root 'static_pages#home'
+  root   'static_pages#home'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users, :except => [:edit, :update, :destroy]
-  get 'signup', to: 'users#new'
 end
