875679d546e507b876f1cf99c19d7bafdeb42ddb
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-02 15:38:51 +0900

    GoodPointモデルにnameカラムのバリデーションを追加

--- app/models/good_point.rb
+++ app/models/good_point.rb
@@ -1,2 +1,5 @@
 class GoodPoint < ApplicationRecord
+#  belongs_to :said_good_point
+
+  validates :name, presence: true
 end
--- test/models/good_point_test.rb
+++ test/models/good_point_test.rb
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class GoodPointTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @good_point = GoodPoint.new(name: "ムードメーカー")
+  end
+
+  test "サンプルデータがvalidでないといけない" do
+    assert @good_point.valid?
+  end
+
+  test "nameは空ではいけない" do
+    @good_point.name = nil
+    assert_not @good_point.valid?
+  end
 end
