d95da07f6a6d7a1f608274fd13e4e375353e7a96
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-02 14:39:45 +0900

    UserとSaidGoodPointモデル間の関連を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  #has_many :finding_good_points, classname:   "SaidGoodPoint",
-  #                               foreign_key: "from",
-  #                               dependente:  :destroy
+   has_many :finding_good_points, class_name: "SaidGoodPoint",
+                                  foreign_key: "from_id",
+                                  dependent:  :destroy
 
   validates :name, presence: true, length: { maximum: 50 },
                    uniqueness: true
