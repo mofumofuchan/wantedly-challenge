72e62764b6004fa2cbbc2b32f83c400a5d39277d
mofumofuchan <you_oki1@yahoo.co.jp>
2016-12-02 21:08:37 +0900

    ユーザ一覧ページへのリンクを表示させるようにした

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,4 +34,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  test "ログインし，ユーザー一覧を表示，最後にログアウト" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { name: @user.name, password: "takami" } }
+
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", index_path
+
+    # ユーザー一覧が表示されているか確認
+    get index_path
+    assert_template 'users/index'
+    
+    # logout
+    delete logout_path
+    assert_not is_logged_in?
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
 end
